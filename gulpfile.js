7ed7c538c3857b59024f6be2d649ff7215b99999
Tod Fleeman <rtoddf@me.com>
2017-11-24 23:11:42 -0500

    added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
+	browserify = require('gulp-browserify'),
 	concat = require('gulp-concat');
 
 // gulp.task('log', function(){
@@ -13,5 +14,6 @@ var jsSources = [
 gulp.task('js', function(){
 	gulp.src(jsSources)
 		.pipe(concat('script.js'))
+		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
 });
