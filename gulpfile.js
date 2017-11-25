460109d9e07a2b4ca99f18017819501ef8c9b970
Tod Fleeman <rtoddf@me.com>
2017-11-24 23:05:35 -0500

    concat js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	concat = require('gulp-concat');
 
-gulp.task('log', function(){
-	gutil.log('grids are awesome');
-})
+// gulp.task('log', function(){
+// 	gutil.log('grids are awesome');
+// });
+
+var jsSources = [
+	'components/scripts/base.js'
+];
+
+gulp.task('js', function(){
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('builds/development/js'))
+});
