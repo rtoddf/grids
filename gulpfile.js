5cbc53fe1b91037b661cb394131f0c30b503ae30
Tod Fleeman <rtoddf@me.com>
2017-12-03 13:59:57 -0500

    gulp watch command

--- gulpfile.js
+++ gulpfile.js
@@ -33,3 +33,20 @@ gulp.task('compass', function(){
 		.on('error', gutil.log)
 		.pipe(gulp.dest('builds/development/css'))
 })
+
+gulp.task('watch', function(){
+	gulp.watch(jsSources, ['js']);
+	gulp.watch('components/sass/*.scss', ['compass']);
+})
+
+gulp.task('default', ['js', 'compass']);
+
+
+
+
+
+
+
+
+
+
