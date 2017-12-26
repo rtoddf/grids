c6b6d899e06c620ce85051587fe389aea5ed83e3
Todd Fleeman <Todd.Fleeman@coxinc.com>
2017-12-26 14:06:37 -0500

    almost done moving

---
+++ builds/development/js/grid-square.js
@@ -0,0 +1,20 @@
+$('.tile').on('click', function(){
+	$('.tile').removeClass('expanded-square')
+	$(this).addClass('expanded-square')
+})
+
+$('img').on('click', function(elem){
+	// if ($(this).parents('.expanded-square').length) {
+	// 	console.log('this: ', $(this).parent().parent())
+	// 	var something = $(this).parent().parent()
+		
+	// }
+	
+
+	$( ".tile" ).each(function( index, b ) {
+		if ($(this).parents('.expanded-square').length) {
+			$(this).removeClass('tile')
+		}
+		console.log($(this));
+	});
+})
